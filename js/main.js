c90ad055d62174394edd3f45f5dc94551c7821b2
Bien Oliver Cucio <51485586+BienDev@users.noreply.github.com>
2022-01-17 20:23:03 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -100,3 +100,12 @@ function init() {
   const wait = txtElement.getAttribute('data-wait');
   new TypeWriter(txtElement, words, wait);
 }
+
+const navbar = document.querySelector('.nav');
+window.onscroll = () => {
+    if (window.scrollY > 100) {
+        navbar.classList.add('nav-active');
+    } else {
+        navbar.classList.remove('nav-active');
+    }
+};
