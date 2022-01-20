86b4838be052d432715f245a2d22cf1ec00625b8
Bien Oliver Cucio <51485586+BienDev@users.noreply.github.com>
2022-01-20 21:19:40 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 const navbar = document.querySelector('.nav');
+const background = document.querySelector('.motion-background');
+
 window.onscroll = () => {
     if (window.scrollY > 100) {
         navbar.classList.add('nav-active');
     } else {
         navbar.classList.remove('nav-active');
+    }
+
+    if (window.scrollY > 600) {
+        background.classList.add('motion-background-opacity');
+    } else {
+        background.classList.remove('motion-background-opacity');
     }
 };
