2a64177212853a49cc916f5d7dec59cc31ae4b9b
Bien Oliver Cucio <51485586+BienDev@users.noreply.github.com>
2022-01-24 21:37:48 +0800

    Add files via upload

---
+++ js/thankyou.js
@@ -0,0 +1,6 @@
+const navbar = document.querySelector('.nav');
+const background = document.querySelector('.motion-background');
+
+navbar.classList.add('nav-active');
+
+background.classList.add('motion-background-opacity');
